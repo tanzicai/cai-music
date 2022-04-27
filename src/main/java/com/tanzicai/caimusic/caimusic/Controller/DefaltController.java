ea8d42b343634f0788119f344f1c516f6620914d
tanzicai <1728561744@qq.com>
2022-04-27 20:26:13 +0800

    初始化cai-music 测试接口hello畅通

---
+++ src/main/java/com/tanzicai/caimusic/caimusic/Controller/DefaltController.java
@@ -0,0 +1,15 @@
+package com.tanzicai.caimusic.caimusic.Controller;
+
+import org.apache.logging.log4j.message.StringFormattedMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testHello")
+public class DefaltController {
+    @GetMapping
+    public String sayHello(){
+        return "通信畅通，SpringWeb正常启动，testHello接口正常返回内容";
+    }
+}
