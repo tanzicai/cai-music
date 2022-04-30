30c41203119ffbf7a3f2f6d099c2a64b16a6162d
tanzicai <1728561744@qq.com>
2022-04-30 18:51:28 +0800

    运行跨域请求

--- src/main/java/com/tanzicai/caimusic/caimusic/Controller/DefaltController.java
+++ src/main/java/com/tanzicai/caimusic/caimusic/Controller/DefaltController.java
@@ -1,10 +1,11 @@
 package com.tanzicai.caimusic.caimusic.Controller;
 
 import org.apache.logging.log4j.message.StringFormattedMessage;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/testHello")
 public class DefaltController {
