74167a4be2fc5112b32c76815750e8e52f24b097
tanzicai <1728561744@qq.com>
2022-05-18 21:59:57 +0800

    create Dto to get Object from other

---
+++ src/main/java/com/tanzicai/caimusic/dto/RoleDto.java
@@ -0,0 +1,10 @@
+package com.tanzicai.caimusic.dto;
+
+public class RoleDto {
+    private String id;
+
+    private String name;
+
+    //显示的名称
+    private String title;
+}
---
+++ src/main/java/com/tanzicai/caimusic/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.tanzicai.caimusic.dto;
+
+import com.tanzicai.caimusic.vo.RoleVo;
+
+import java.util.List;
+
+public class UserDto {
+
+    private String username;
+
+    private List<RoleVo> roles;
+
+    private String nickname;
+
+    private String password;
+}
